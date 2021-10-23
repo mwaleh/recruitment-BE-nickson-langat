e7df0f9428cb10c2cec72d119bbcde31aa7bf016
nicksonlangat <nicksonlangat95.com>
2021-10-23 16:58:03 +0300

    increase uploads

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,3 +144,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL= 'accounts.User'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10*1024*1024
